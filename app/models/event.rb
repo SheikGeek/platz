04eea4723bb384bd2a9b3060c6ed9dd19d3cd179
Square <github@squareup.com>
2013-01-11 10:33:03 -0800

    Added new columns to the events model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ActiveRecord::Base
+  attr_accessible :google_event_id, :survey_url
   belongs_to :creator, :class_name => "User"
   has_many :event_attendees
   has_many :attendees, :through => :event_attendees, :source => :user
